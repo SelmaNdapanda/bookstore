750b603a71eb461866fdfeb05dc861d32f279eec
Selma <61165443+SelmaNdapanda@users.noreply.github.com>
2022-11-03 15:47:26 +0200

    Create book page

---
+++ src/pages/Books.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Bookitem from '../components/Bookitem';
+import Addbook from '../components/Addbook';
+
+const Books = () => {
+  return (
+    <div>
+      <Bookitem />
+      <Addbook />
+    </div>
+  );
+}
+
+export default Books;
