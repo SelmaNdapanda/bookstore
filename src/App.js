aa4df4e4676c6c991147c2deb731b19aa16220c0
Selma <61165443+SelmaNdapanda@users.noreply.github.com>
2022-11-03 16:15:05 +0200

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Books from './pages/Books';
 import './App.css';
+import Navbar from './components/Navbar';
+import Categories from './pages/Categories';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Books />
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/Categories" element={<Categories />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
