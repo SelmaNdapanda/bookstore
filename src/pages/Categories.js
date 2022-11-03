837f1063c00b76b0cf3b68932f0f1b555a03fa48
Selma <61165443+SelmaNdapanda@users.noreply.github.com>
2022-11-03 15:56:34 +0200

    Create Categories page

---
+++ src/pages/Categories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Categories = () => {
+  return (
+    <div className='categories'>
+      <button type="button">Check Status</button>
+    </div>
+  );
+}
+
+export default Categories;
