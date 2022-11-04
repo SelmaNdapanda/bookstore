95451474eca5c3365bb7da485d043390455374fd
Selma <61165443+SelmaNdapanda@users.noreply.github.com>
2022-11-04 12:14:36 +0200

    Update Book.js

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import { React, useState } from 'react';
 import Bookitem from '../components/Bookitem';
 import Addbook from '../components/Addbook';
 
-const Books = () => (
+function Books() {
+  const [books] = useState([{id:'1', category: 'action', title: 'The Hunger Game', author: 'Suzanne Collins'}]);
+  return (
+    <>
   <div>
-    <Bookitem />
+    {books.map((book) => <Bookitem key={book.id} category={book.category} title={book.title} author={book.author} />)}
     <Addbook />
   </div>
-);
+  </>
+)
+};
 
 export default Books;
