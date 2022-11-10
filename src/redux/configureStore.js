250ac7ae1f4ec987baedc651a2432b1587bb866f
Selma <61165443+SelmaNdapanda@users.noreply.github.com>
2022-11-10 10:59:39 +0200

    Apply middleware

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
 import bookReducer from './books/books';
 import categoryReducer from './categories/categories';
+import thunk from 'redux-thunk';
 
 const store = configureStore({
   reducer: {
     books: bookReducer,
     categories: categoryReducer,
   },
-});
+}, applyMiddleware(thunk));
 
 export default store;
