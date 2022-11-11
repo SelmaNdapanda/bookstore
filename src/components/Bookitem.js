903b4691811a54872e3623453c3d06e688c7fe41
Selma <61165443+SelmaNdapanda@users.noreply.github.com>
2022-11-11 16:10:45 +0200

    Modify Bookiem.js

--- src/components/Bookitem.js
+++ src/components/Bookitem.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { CircularProgressbar } from 'react-circular-progressbar';
-import { removeBook } from '../redux/books/books';
 import 'react-circular-progressbar/dist/styles.css';
+import { removeBook } from '../redux/books/books';
 
 const Bookitem = (props) => {
   const {
